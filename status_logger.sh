7cae27e0a6da7bfa5cadae38578b93e74c38779f
Mohsen <moohamadiann@gmail.com>
2025-06-28 21:13:47 +0330

    Find cpu usage and load agerage of system

--- status_logger.sh
+++ status_logger.sh
@@ -6,3 +6,7 @@ kernel_release=$(uname -r)
 os_version="$(lsb_release -d -s | awk 'NR==1 {print $1 "-" $2 "-" $3}')"
 
 online_users=$(who | awk '{print $1}' | sort | uniq -c | awk '{printf "%s:%s", $2,$1}')
+
+cpu_usage=$(top -bn2 | grep '%CPU' | tail -1 | awk '{print 100-$8}')
+
+load_average=$(uptime | awk '{print $8 $9 $10}')
