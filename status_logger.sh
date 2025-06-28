c0d28bc3718d76ef3a6e07c2be30a96aa5d7a731
Mohsen <moohamadiann@gmail.com>
2025-06-28 21:20:52 +0330

    Fix: patern of grep command

--- status_logger.sh
+++ status_logger.sh
@@ -7,7 +7,7 @@ os_version="$(lsb_release -d -s | awk 'NR==1 {print $1 "-" $2 "-" $3}')"
 
 online_users=$(who | awk '{print $1}' | sort | uniq -c | awk '{printf "%s:%s", $2,$1}')
 
-cpu_usage=$(top -bn2 | grep '%CPU' | tail -1 | awk '{print 100-$8}')
+cpu_usage=$(top -bn2 | grep '%Cpu' | tail -1 | awk '{print 100-$8}')
 
 load_average=$(uptime | awk '{print $8 $9 $10}')
 
