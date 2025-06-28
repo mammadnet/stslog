0312ac884bbedb2908376997af3b57cc80b198d2
Mohsen <moohamadiann@gmail.com>
2025-06-28 16:58:55 +0330

    Store online user and number of each user session

--- status_logger.sh
+++ status_logger.sh
@@ -4,3 +4,5 @@ kernel_release=$(uname -r)
 
 
 os_version="$(lsb_release -d -s | awk 'NR==1 {print $1 "-" $2 "-" $3}')"
+
+online_users=$(who | awk '{print $1}' | sort | uniq -c | awk '{printf "%s:%s", $2,$1}')
