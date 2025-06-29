5c4cd0278340d8e495c823fde35b110d57cdc317
Mohsen <moohamadiann@gmail.com>
2025-06-29 15:53:24 +0330

    Check if script run with root user

--- logger.sh
+++ logger.sh
@@ -3,6 +3,12 @@
 log_file_path="/var/log/status.log"
 data_fetcher="data_fetcher.sh"
 
+if [[ $EUID -ne 0 ]]; then
+    echo "$0: Permission denied"
+    echo "$0: Try with ROOT user"
+    exit 1
+fi
+
 if [[ ! -f /var/log/status.log ]]; then
     touch "$log_file_path"
 fi
