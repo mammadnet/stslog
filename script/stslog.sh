a89c659280f55125c8a79d109209228a790859c2
Mohsen <moohamadiann@gmail.com>
2025-07-01 20:06:46 +0330

    Add terminal options to stslog command Show current status without any options and with -s options Change cronjob cycle time with -e option Show help text with -h options and with uncorrent command

--- script/stslog.sh
+++ script/stslog.sh
@@ -6,15 +6,56 @@ data_fetcher="data_fetcher.sh"
 script_path=$(readlink -f "$0")
 script_directory=$(realpath $(dirname "$script_path"))
 
+show_current_status() {
 
-("$script_directory/$data_fetcher") | awk '{
-for (i=4; i<=NF; i++){
-    if (i == 4)
-        printf "%s", $i
-    else if ($i == "MB")
-        printf " MB";
-    else
-        printf "\n%s", $i;
-    }
-    printf "\n"
-}'
+    ("$script_directory/$data_fetcher") | awk '{
+    for (i=4; i<=NF; i++){
+        if (i == 4)
+            printf "%s", $i
+        else if ($i == "MB")
+            printf " MB";
+        else
+            printf "\n%s", $i;
+        }
+        printf "\n"
+    }'
+    return 0
+}
+
+help() {
+    # TODO Show help text for stslog command
+    echo help text for stslog command
+}
+
+if [[ -z $1 ]]; then
+    show_current_status
+fi
+
+while getopts ":e:sh" opt; do
+
+    case $opt in 
+
+        s)
+            show_current_status
+            ;;
+        e)
+            if [[ ! $EUID -eq 0 ]]; then
+                echo This operation requires ROOT permission
+                exit 1
+            fi
+            $script_directory/cronjob_set.sh -e $OPTARG $script_directory/logger.sh
+            
+            if [[ ! $? -eq 0 ]]; then
+                exit 1
+            fi
+            ;;
+        h)
+            help
+            ;;
+        \?)
+            help
+            exit 1
+            ;;
+    esac    
+
+done
